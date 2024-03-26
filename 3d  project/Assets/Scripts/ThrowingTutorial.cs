8b7feb0e5ace6177fb5960581cb0eea4612f536a
yilinLiu08 <yliu89@pratt.edu>
2024-03-27 02:11:55 +0800

    Throw 3 kinds of ball

--- 3d  project/Assets/Scripts/ThrowingTutorial.cs
+++ 3d  project/Assets/Scripts/ThrowingTutorial.cs
@@ -8,7 +8,7 @@ public class ThrowingTutorial : MonoBehaviour
     [Header("References")]
     public Transform cam;
     public Transform attackPoint;
-    public GameObject objectToThrow;
+    public GameObject[] objectsToThrow; 
 
     [Header("Settings")]
     public int totalThrows;
@@ -16,19 +16,27 @@ public class ThrowingTutorial : MonoBehaviour
 
     [Header("Throwing")]
     public KeyCode throwKey = KeyCode.Mouse0;
+    public KeyCode switchKey = KeyCode.Alpha1; 
     public float throwForce;
     public float throwUpwardForce;
 
-    bool readyToThrow;
+    private int currentObjectIndex; 
+    private bool readyToThrow;
 
     private void Start()
     {
         readyToThrow = true;
+        currentObjectIndex = 0;
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0)
+        if (Input.GetKeyDown(switchKey)) 
+        {
+            currentObjectIndex = (currentObjectIndex + 1) % objectsToThrow.Length; 
+        }
+
+        if (Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0)
         {
             Throw();
         }
@@ -39,29 +47,25 @@ public class ThrowingTutorial : MonoBehaviour
         readyToThrow = false;
 
         
-        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
+        GameObject projectile = Instantiate(objectsToThrow[currentObjectIndex], attackPoint.position, cam.rotation);
 
-        
         Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
 
-        
         Vector3 forceDirection = cam.transform.forward;
 
         RaycastHit hit;
 
-        if(Physics.Raycast(cam.position, cam.forward, out hit, 500f))
+        if (Physics.Raycast(cam.position, cam.forward, out hit, 500f))
         {
             forceDirection = (hit.point - attackPoint.position).normalized;
         }
 
-        
         Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
 
         projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
 
         totalThrows--;
 
-        
         Invoke(nameof(ResetThrow), throwCooldown);
     }
 
