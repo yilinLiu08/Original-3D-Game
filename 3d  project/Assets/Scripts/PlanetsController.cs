635101a96e55a02f3a9f0833503f8973518d246e
yilinLiu08 <yliu89@pratt.edu>
2024-04-24 01:58:39 +0800

    Add props to lab and lab Game mechanics level update
    Add props to lab
    lab Game mechanics level update

---
+++ 3d  project/Assets/Scripts/PlanetsController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PlanetsController : MonoBehaviour
+{
+    public PositionController[] controllers; 
+
+    private bool allPositionsAndSizesReached = false;
+
+    void Update()
+    {
+        CheckAllPositionsAndSizes();
+    }
+
+    void CheckAllPositionsAndSizes()
+    {
+        foreach (PositionController controller in controllers)
+        {
+            if (!controller.sizeAndPositionReached)
+            {
+                if (allPositionsAndSizesReached)
+                {
+                    
+                    allPositionsAndSizesReached = false;
+                }
+                return;
+            }
+        }
+
+        
+        if (!allPositionsAndSizesReached)
+        {
+            Debug.Log("All planets true!");
+            allPositionsAndSizesReached = true;
+        }
+    }
+}
